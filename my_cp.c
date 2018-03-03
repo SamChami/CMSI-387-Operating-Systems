773ea6843625e4ba4bf9337397ae614f80e90899
samChami <sc01914@gmail.com>
2018-03-02 23:35:12 -0800

    Updated cp

--- my_cp.c
+++ my_cp.c
@@ -5,31 +5,26 @@
 
 #include <fcntl.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     char buffer[1024];
     int files[2];
     ssize_t count;
 
-    /* Check for insufficient parameters */
-
     files[0] = open(argv[1], O_RDONLY);
     files[1] = open(argv[2], O_RDWR | O_CREAT, 0666);
 
-    if (files[1] == -1) /* Check if file opened (permissions problems ...) */
-    {
-        char fileName[2048];
+    if (files[1] == -1) {
+        char fileName[1024];
         strcat(fileName, argv[2]);
         if ( fileName[strlen(fileName)-1] != '/' ) { 
            strcat(fileName, "/");
         }
+        char* file = strrchr(argv[1], '/');
+        strcat(fileName, file + 1);
 
-        printf("%s\n", fileName);
+        close(file[1]);
         files[1] = open(fileName, O_RDWR | O_CREAT, 0666);
 
-        printf("NO\n");
-
-  
     }
 
     while ((count = read(files[0], buffer, sizeof(buffer))) != 0) {
